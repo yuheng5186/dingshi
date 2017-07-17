5c91cd75296cf63262042e7cd819d43ab1ac78eb
apple <2267369297@qq.com>
2017-07-17 16:45:45 +0800

    Initial Commit

---
+++ carWash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  carWash
+//
+//  Created by Wuxinglin on 2017/7/17.
+//  Copyright © 2017年 DS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ carWash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  carWash
+//
+//  Created by Wuxinglin on 2017/7/17.
+//  Copyright © 2017年 DS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
